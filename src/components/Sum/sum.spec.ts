9d4b88e3dd052f7a48420b6f1aa0611db4f2e260
Johnny <jim850221@gmail.com>
2021-11-10 11:27:16 +0800

    finish the config of jest and react-testesting-lib with jest-dom

---
+++ src/components/Sum/sum.spec.ts
@@ -0,0 +1,5 @@
+import { sum } from './sum';
+
+it('suming 5 and 2 will be 7 ', () => {
+    expect(sum(2,5)).toBe(7);
+})
---
+++ src/jest.setup.ts
@@ -0,0 +1,2 @@
+// this file will be executed before any test suite.
+import '@testing-library/jest-dom'
